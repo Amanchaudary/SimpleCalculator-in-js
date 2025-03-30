d3556f0f34bc2bfbbba01a3bc0b496a2fed986a8
amanecdy <amanecdy@gmail.com>
2025-03-30 09:48:18 +0900

    index.html and style.css file create and join eachother

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-//  this is a simple calculator by using javaScript only
+//  this is a simple calculator by using javaScript css and html
+// An Easy Tutorial on How to Build A Calculator App in JavaScript in Hindi | simple calculator in JS
