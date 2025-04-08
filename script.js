385f056d67860e3e7589928d6c64e6e119cd17f3
amanecdy <amanecdy@gmail.com>
2025-04-08 11:35:23 +0900

    simple calculater formate photo save and make button for number

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //  this is a simple calculator by using javaScript css and html
+
+// tutorial
+// Creating a Calculator using HTML, CSS & JavaScript 
 // An Easy Tutorial on How to Build A Calculator App in JavaScript in Hindi | simple calculator in JS
+
