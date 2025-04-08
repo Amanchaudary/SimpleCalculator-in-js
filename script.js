16e0d8816173669970c1441277df573c8ac4dbb6
amanecdy <amanecdy@gmail.com>
2025-04-08 12:37:14 +0900

    calculator finesh

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 //  this is a simple calculator by using javaScript css and html
 
 // tutorial
-// Creating a Calculator using HTML, CSS & JavaScript 
+// Creating a Calculator using HTML, CSS & JavaScript
 // An Easy Tutorial on How to Build A Calculator App in JavaScript in Hindi | simple calculator in JS
 
+let String = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == '='){
+      String = eval(String);
+      document.querySelector("input").value = String;
+    } 
+    else if (e.target.innerHTML== 'C') {
+      String = "";
+      document.querySelector("input").value = String;
+    } 
+    
+    else { 
+      console.log(e.target);
+      String = String + e.target.innerHTML;
+      document.querySelector("input").value = String;
+    }
+  });
+});
